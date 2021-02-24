162686da27fd10aa340b4d1501675de060bd1399
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-02-24 14:25:48 -0300

    sdm660-common: Introduce Dirac Sound
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -430,3 +430,4 @@ PRODUCT_BOOT_JARS += \
 
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/sdm660-common/sdm660-common-vendor.mk)
+$(call inherit-product, vendor/xiaomi/dirac/dirac.mk)
