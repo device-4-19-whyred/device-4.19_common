70f8b53352e3063c79faea34d2aab844b26e739e
CHRISL7 <70589286+ChrisW444@users.noreply.github.com>
2021-03-04 14:46:44 -0400

    sdm660-common: use personal qcom_power

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,8 +4,10 @@ export SKIP_ABI_CHECKS=true
 # rm -rf
 rm -rf vendor/codeaurora/telephony
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
+rm -rf vendor/qcom/opensource/power
 
 # clone
 git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
 git clone https://github.com/ChrisW444/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
 git clone https://github.com/ChrisW444/vendor_xiaomi_dirac vendor/xiaomi/dirac
+git clone https://github.com/ChrisW444/android_vendor_qcom_opensource_power-1 vendor/qcom/opensource/power
