2ede733b33a7a60d37090e373336e951f304b4f3
CHRISL7 <70589286+ChrisW444@users.noreply.github.com>
2021-03-06 19:01:03 -0400

    sdm660-common: use custom hals

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -15,3 +15,6 @@ git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git
 git clone https://github.com/ChrisW444/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
 git clone https://github.com/ChrisW444/vendor_xiaomi_dirac vendor/xiaomi/dirac
 git clone https://github.com/ChrisW444/android_vendor_qcom_opensource_power-1 vendor/qcom/opensource/power
+git clone https://github.com/ChrisW444/android_hardware_qcom_display hardware/qcom-caf/sdm660/display
+git clone https://github.com/ChrisW444/android_hardware_qcom_audio hardware/qcom-caf/sdm660/audio
+git clone https://github.com/ChrisW444/android_hardware_qcom_media hardware/qcom-caf/sdm660/media
