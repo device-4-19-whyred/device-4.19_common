af014595dbb25d3fd75ad9757352a3021de6a0e7
CHRISL7 <70589286+ChrisW444@users.noreply.github.com>
2021-03-19 08:06:52 -0400

    sdm660-common: track clang 12.0.3

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,10 +8,11 @@ rm -rf vendor/codeaurora/telephony
 rm -rf vendor/qcom/opensource/power
 
 # clone
-git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
+#git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
 git clone https://github.com/ChrisW444/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
 git clone https://github.com/ChrisW444/vendor_xiaomi_dirac vendor/xiaomi/dirac
 git clone https://github.com/ChrisW444/android_vendor_qcom_opensource_power-1 vendor/qcom/opensource/power
 git clone https://github.com/ChrisW444/android_hardware_qcom_display hardware/qcom-caf/sdm660/display
 git clone https://github.com/ChrisW444/android_hardware_qcom_audio hardware/qcom-caf/sdm660/audio
 git clone https://github.com/ChrisW444/android_hardware_qcom_media hardware/qcom-caf/sdm660/media
+git clone https://github.com/SDM660-Project/prebuilts_clang_host_linux-x86_clang-r412851 prebuilts/clang/host/linux-x86/clang-r412851
