297bbedae61ed8ab7390daed3c31ada5c988d504
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-03-19 20:49:31 -0300

    sdm660-common: Use display config 1.0 * dependency of liboemcrypto Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -210,7 +210,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
     android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.1
+    vendor.display.config@1.0
 
 # DRM
 PRODUCT_PACKAGES += \
