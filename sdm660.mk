11d46a309bf8bab6f46e2723b2e027c4e9ad9a2c
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-04-02 16:32:44 -0300

    sdm660-common: Build wfd dependency blobs from source * Avoid manifest entry for old blobs compatibility * Must use display hal from https://github.com/SDM660-Project/android_hardware_qcom_display * Patch libwfdmmsink.so * Fix wfdservice to work on android-11.0.0_r19
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- sdm660.mk
+++ sdm660.mk
@@ -203,9 +203,14 @@ PRODUCT_PACKAGES += \
     hwcomposer.sdm660 \
     memtrack.sdm660 \
     libdisplayconfig \
+    libdisplayconfig.qti \
+    libdisplayconfig.qti.vendor \
     libtinyxml \
     libqdMetaData \
-    libqdMetaData.system
+    libqdMetaData.system \
+    libstagefright_enc_common \
+    vendor.display.config@2.0 \
+    vendor.display.config@2.0.vendor
 
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0_32 \
@@ -216,8 +221,7 @@ PRODUCT_PACKAGES += \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     android.hardware.renderscript@1.0-impl \
-    android.frameworks.displayservice@1.0 \
-    vendor.display.config@1.0
+    android.frameworks.displayservice@1.0
 
 # DRM
 PRODUCT_PACKAGES += \
