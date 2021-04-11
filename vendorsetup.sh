75398799bc1ce3237d429bdc7ff704b0146f2b18
predator112 <65210380+predator112@users.noreply.github.com>
2021-04-11 12:16:29 +0700

    remove hal

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,13 +4,11 @@ export SELINUX_IGNORE_NEVERALLOWS=true
 export TEMPORARY_DISABLE_PATH_RESTRICTIONS=true
 
 # rm -rf
-rm -rf vendor/codeaurora/telephony
 rm -rf vendor/qcom/opensource/power
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 
 # clone
 #git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
-git clone https://github.com/ChrisW444/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
 git clone https://github.com/SDM660-Project/android_vendor_qcom_opensource_power vendor/qcom/opensource/power
 git clone https://github.com/SDM660-Project/android_hardware_qcom_display hardware/qcom-caf/sdm660/display
 git clone https://github.com/SDM660-Project/android_hardware_qcom_audio hardware/qcom-caf/sdm660/audio
