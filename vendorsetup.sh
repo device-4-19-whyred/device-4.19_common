5ec70061e839be59c67ee8d71f42561c191db623
predator112 <65210380+predator112@users.noreply.github.com>
2021-04-11 12:19:26 +0700

    new repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,9 +9,9 @@ rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 
 # clone
 #git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
-git clone https://github.com/SDM660-Project/android_vendor_qcom_opensource_power vendor/qcom/opensource/power
-git clone https://github.com/SDM660-Project/android_hardware_qcom_display hardware/qcom-caf/sdm660/display
-git clone https://github.com/SDM660-Project/android_hardware_qcom_audio hardware/qcom-caf/sdm660/audio
-git clone https://github.com/SDM660-Project/android_hardware_qcom_media hardware/qcom-caf/sdm660/media
+git clone https://github.com/device-4-19-whyred/android_vendor_qcom_opensource_power vendor/qcom/opensource/power
+git clone https://github.com/device-4-19-whyred/android_hardware_qcom_display -b sdm660-4.19 hardware/qcom-caf/sdm660/display
+git clone https://github.com/device-4-19-whyred/android_hardware_qcom_audio -b sdm660-4.19 hardware/qcom-caf/sdm660/audio
+git clone https://github.com/device-4-19-whyred/android_hardware_qcom_media  -b sdm660-4.19 hardware/qcom-caf/sdm660/media
 git clone https://github.com/SDM660-Project/prebuilts_clang_host_linux-x86_clang-r412851 prebuilts/clang/host/linux-x86/clang-r412851
-git clone https://github.com/SDM660-Project/android_vendor_qcom_opensource_data-ipa-cfg-mgr vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://github.com/device-4-19-whyred/android_vendor_qcom_opensource_data-ipa-cfg-mgr vendor/qcom/opensource/data-ipa-cfg-mgr
