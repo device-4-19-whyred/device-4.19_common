0eaace3f0180790b4a12be57f8beae33fb6da727
Ubuntu <iqbal@iqbal.heymnpl2vh0ujozbrymtoeynvb.cwx.internal.cloudapp.net>
2021-04-11 07:39:48 +0000

    face unlock

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,7 +8,7 @@ rm -rf vendor/qcom/opensource/power
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 
 # clone
-#git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
+git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
 git clone https://github.com/device-4-19-whyred/android_vendor_qcom_opensource_power vendor/qcom/opensource/power
 git clone https://github.com/device-4-19-whyred/android_hardware_qcom_display -b sdm660-4.19 hardware/qcom-caf/sdm660/display
 git clone https://github.com/device-4-19-whyred/android_hardware_qcom_audio -b sdm660-4.19 hardware/qcom-caf/sdm660/audio
